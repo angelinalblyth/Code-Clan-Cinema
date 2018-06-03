7fbcc632bdbb84f78cefee52f2662d3e8b0c7181
Angelina Blyth <angelinalblyth@hotmail.com>
2018-06-03 21:35:29 +0100

    Added tickets sold to screening

--- db/cinema.sql
+++ db/cinema.sql
@@ -19,12 +19,13 @@ CREATE TABLE films(
 CREATE TABLE screenings(
   id SERIAL4 PRIMARY KEY,
   screening_time VARCHAR(255),
-  film_id INT4 REFERENCES films(id) ON DELETE CASCADE
+  film_id INT4 REFERENCES films(id) ON DELETE CASCADE,
+  tickets_sold INT4
 );
 
 CREATE TABLE tickets(
   id SERIAL4 PRIMARY KEY,
   customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
-  film_id INT4 REFERENCES films(id) ON DELETE CASCADE,
+  --film_id INT4 REFERENCES films(id) ON DELETE CASCADE,
   screening_id INT4 REFERENCES screenings(id) ON DELETE CASCADE
 );
--- models/ticket.rb
+++ models/ticket.rb
@@ -6,18 +6,19 @@ require_relative("customer.rb")
 class Ticket
 
   attr_reader :id
-  attr_accessor :customer_id, :film_id
+  attr_accessor :customer_id, :screening_id
 
   def initialize(options)
     @id = options['id'][0].to_i if options['id']
     @customer_id = options['customer_id'].to_i
-    @film_id = options ['film_id'].to_i
+    #@film_id = options['film_id'].to_i
     #add screening id?
+    @screening_id = options[:screening_id].to_i
   end
 
   def save()
-    sql = "INSERT INTO tickets (customer_id, film_id) VALUES ($1, $2) RETURNING id"
-    values = [@customer_id, @film_id]
+    sql = "INSERT INTO tickets (customer_id, screening_id) VALUES ($1, $2) RETURNING id"
+    values = [@customer_id, @screening_id]
     ticket = SqlRunner.run(sql, values).first
     @id = ticket['id'].to_i
   end
@@ -35,8 +36,8 @@ class Ticket
   end
 
   def update()
-    sql = "UPDATE tickets SET (customer_id, film_id) = ($1, $2) WHERE id = $3"
-    values = [@customer_id, @film_id, @id]
+    sql = "UPDATE tickets SET (customer_id, screening_id) = ($1, $2) WHERE id = $3"
+    values = [@customer_id, @screening_id, @id]
     SqlRunner.run(sql, values)
   end
 
